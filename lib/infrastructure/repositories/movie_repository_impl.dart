36da58e38f7944d8b63670881662340cb0082f68
Jose Roncal <joseluis.rome@protonmail.com>
2024-01-21 20:47:13 -0500

    feat: :sparkles: get movies data from moviedb and display

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repositories.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDataSource dataSource;
+  MovieRepositoryImpl(this.dataSource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return dataSource.getNowPlaying(page: page);
+  }
+}
